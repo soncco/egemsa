8a068a103938a320e07fb09b5d5cbb58ba82d387
brau <soncco@gmail.com>
2017-08-02 09:21:55 -0500

    Settings

--- egemsa/urls.py
+++ egemsa/urls.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls.static import static
 
 import settings
 
@@ -9,7 +8,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^', include('front.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 admin.site.site_header = 'Transparencia - Administración'
 admin.site.site_title = 'Transparencia - Administración'
