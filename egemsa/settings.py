d7a49d02971a57c25707d9da64b728b90fd5537e
Braulio Soncco <soncco@gmail.com>
2021-11-02 20:59:45 -0500

    static

--- egemsa/settings.py
+++ egemsa/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '../static/'
+STATIC_ROOT = config('STATIC')
 
 MEDIA_ROOT = '../static/archivos/'
 #MEDIA_URL = '/static/archivos/'
