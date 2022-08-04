265ec47d34f2431944b4869a5922a4eaa5a84520
Braulio Soncco <soncco@gmail.com>
2022-08-04 07:07:44 -0500

    filtro de fechas

--- front/static/front/js/scripts.js
+++ front/static/front/js/scripts.js
@@ -1,60 +1,64 @@
-var elems = document.querySelectorAll('.dtp');
-var fecha = document.getElementById('fecha');
-var desde = document.getElementById('desde');
-var hasta = document.getElementById('hasta');
+var elems = document.querySelectorAll(".dtp");
+var fecha = document.getElementById("fecha");
+var fecha_final = document.getElementById("fecha_final");
+var desde = document.getElementById("desde");
+var hasta = document.getElementById("hasta");
 
-elems.forEach(function(el) {
+elems.forEach(function (el) {
   var datepicker = new Datepicker(el, {
-    language: 'es',
-    format: 'dd M yyyy'
+    language: "es",
+    format: "dd M yyyy",
   });
-  el.addEventListener('changeDate', function(e) {
-    switch(e.target.id) {
-      case 'datepicker':
-        fecha.value = e.detail.date.getTime()/1000;
-      break;
-      case 'desdep':
-        desde.value = e.detail.date.getTime()/1000;
-      break;
-      case 'hastap':
-        hasta.value = e.detail.date.getTime()/1000;
-      break;
+  el.addEventListener("changeDate", function (e) {
+    switch (e.target.id) {
+      case "datepicker":
+        fecha.value = e.detail.date.getTime() / 1000;
+        break;
+      case "datepicker2":
+        fecha_final.value = e.detail.date.getTime() / 1000;
+        break;
+      case "desdep":
+        desde.value = e.detail.date.getTime() / 1000;
+        break;
+      case "hastap":
+        hasta.value = e.detail.date.getTime() / 1000;
+        break;
       default:
     }
   });
 });
 
-var f = document.getElementById('busqueda-form');
-if(f) {
-  f.addEventListener('submit', function(e) {
-    if(desde.value == '' && hasta.value != '') {
-      alert('Ingresa la fecha inicial');
+var f = document.getElementById("busqueda-form");
+if (f) {
+  f.addEventListener("submit", function (e) {
+    if (desde.value == "" && hasta.value != "") {
+      alert("Ingresa la fecha inicial");
       desde.focus();
-      e.preventDefault()
+      e.preventDefault();
       return false;
     }
 
-    if(desde.value != '' && hasta.value != '') {
+    if (desde.value != "" && hasta.value != "") {
       var desdeval = desde.value * 1;
       var hastaval = hasta.value * 1;
-      if(hastaval < desdeval) {
-        alert('Corrige el rango de fechas.');
+      if (hastaval < desdeval) {
+        alert("Corrige el rango de fechas.");
         desde.focus();
-        e.preventDefault()
+        e.preventDefault();
         return false;
       }
     }
   });
 }
 
-var pdfLinks = document.querySelectorAll('.show-pdf');
+var pdfLinks = document.querySelectorAll(".show-pdf");
 
-pdfLinks.forEach(function(el) {
-  el.addEventListener('click', function(e) {
+pdfLinks.forEach(function (el) {
+  el.addEventListener("click", function (e) {
     e.preventDefault();
-    var myModal = new bootstrap.Modal(document.getElementById('commonModal'));
-    var iframePDF = document.getElementById('iframepdf');
+    var myModal = new bootstrap.Modal(document.getElementById("commonModal"));
+    var iframePDF = document.getElementById("iframepdf");
     iframePDF.src = e.target.href;
     myModal.show();
-  })
+  });
 });
--- front/views.py
+++ front/views.py
@@ -1,3 +1,4 @@
+import sys
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
@@ -10,7 +11,6 @@ from .utils import fechas, get_fecha
 from datetime import datetime
 
 
-
 def index(request):
     categorias = cache.get('categorias:padres')
     if categorias is None:
@@ -19,12 +19,13 @@ def index(request):
 
     documentos = cache.get('documentos:ultimos')
     if documentos is None:
-        documentos = Documento.objects.filter(enlace = None).order_by('-id')[:10]
+        documentos = Documento.objects.filter(enlace=None).order_by('-id')[:10]
         cache.set('documentos:ultimos', documentos)
 
     agendas = Agenda.hoy.eventos_hoy()
 
-    context = {'categorias': categorias, 'documentos': documentos, 'agendas': agendas}
+    context = {'categorias': categorias,
+               'documentos': documentos, 'agendas': agendas}
     return render(request, 'front/index.html', context)
 
 
@@ -46,14 +47,15 @@ def categoria(request, slug):
 def documento(request, id):
     documento = cache.get('documentos:pk:%s' % str(id))
     if documento is None:
-        documento = get_object_or_404(Documento, pk = id)
+        documento = get_object_or_404(Documento, pk=id)
         cache.set('documentos:pk:%s' % str(id), documento)
 
     documentos = cache.get('documentos:categoria:%d' % documento.categoria.pk)
     if documentos is None:
-        documentos = Documento.objects.filter(categoria = documento.categoria)
-        documentos = documentos.filter(~Q(pk = documento.pk))[:5]
-        cache.set('documentos:categoria:%d' % documento.categoria.pk, documentos)
+        documentos = Documento.objects.filter(categoria=documento.categoria)
+        documentos = documentos.filter(~Q(pk=documento.pk))[:5]
+        cache.set('documentos:categoria:%d' %
+                  documento.categoria.pk, documentos)
 
     context = {'documento': documento, 'documentos': documentos}
     return render(request, 'front/documento.html', context)
@@ -81,47 +83,61 @@ def agenda(request, id):
     today = datetime.now().date()
 
     fecha = request.GET.get('fecha')
-    if fecha is not None:
-        fecha, fecha_end, fecha_start = fechas(fecha)
-        agendas = Agenda.objects.filter(Q(dirige = participante) | Q(participan = participante), fecha_hora__lte=fecha_end, fecha_hora__gte=fecha_start).order_by('fecha_hora')
+    fecha_final = request.GET.get('fecha_final')
+    if fecha is not None and fecha_final is not None:
+        print('COMPLETO')
+        _fecha = get_fecha(fecha)
+        _fecha_final = get_fecha(fecha_final)
+        fecha = datetime.fromtimestamp(int(fecha))
+        fecha_final = datetime.fromtimestamp(int(fecha_final))
+        agendas = Agenda.objects.filter(Q(dirige=participante) | Q(
+            participan=participante), fecha_hora__range=(_fecha, _fecha_final)).order_by('fecha_hora')
     else:
+        print('NO COMPLETO')
         fecha = today
-        agendas = Agenda.hoy.eventos_hoy().filter(Q(dirige = participante) | Q(participan = participante))
+        fecha_final = today
+        agendas = Agenda.hoy.eventos_hoy().filter(
+            Q(dirige=participante) | Q(participan=participante))
 
     agendas = agendas.distinct()
 
-    context = {'categorias': categorias, 'agendas': agendas, 'fecha': fecha, 'participantes': participantes, 'actual': participante}
+    context = {'categorias': categorias, 'agendas': agendas, 'fecha': fecha, 'fecha_final': fecha_final,
+               'participantes': participantes, 'actual': participante}
+    print(context['fecha'], context['fecha_final'])
     return render(request, 'front/agenda.html', context)
 
+
 def evento(request, id):
-    evento = get_object_or_404(Agenda, pk = id)
+    evento = get_object_or_404(Agenda, pk=id)
     context = {'evento': evento}
     return render(request, 'front/evento.html', context)
+
 
 def buscar(request):
     q = request.GET.get('q', '')
     first = request.GET.get('first')
     categorias = Categoria.padres.all()
-    documentos = Documento.objects.filter(Q(nombre__icontains = q) | Q(nombre__icontains = q))
+    documentos = Documento.objects.filter(
+        Q(nombre__icontains=q) | Q(nombre__icontains=q))
 
     if first != '':
         cats = request.GET.getlist('categoria[]')
         if len(cats) > 0:
-            documentos = documentos.filter(Q(categoria__padre__padre__padre__pk__in = cats) | Q(categoria__padre__padre__pk__in = cats) | Q(categoria__padre__pk__in = cats) | Q(categoria__pk__in = cats))
+            documentos = documentos.filter(Q(categoria__padre__padre__padre__pk__in=cats) | Q(
+                categoria__padre__padre__pk__in=cats) | Q(categoria__padre__pk__in=cats) | Q(categoria__pk__in=cats))
 
         desde = request.GET.get('desde')
         hasta = request.GET.get('hasta')
         if hasta == '' and desde != '':
             desde = get_fecha(desde)
-            documentos = documentos.filter(fecha__gte = desde)
+            documentos = documentos.filter(fecha__gte=desde)
         elif hasta != '' and desde != '':
             desde = get_fecha(desde)
             hasta = get_fecha(hasta)
-            documentos = documentos.filter(fecha__range = (desde, hasta))
+            documentos = documentos.filter(fecha__range=(desde, hasta))
         else:
             desde = None
             hasta = None
-
 
     page = request.GET.get('page', 1)
     paginator = Paginator(documentos, 10)
@@ -134,24 +150,27 @@ def buscar(request):
         documentos = paginator.page(paginator.num_pages)
 
     if first != '':
-        cats = [ int(x) for x in cats ]
-        context = {'categorias': categorias, 'documentos': documentos, 'cats': cats, 'desde': desde, 'hasta': hasta}
+        cats = [int(x) for x in cats]
+        context = {'categorias': categorias, 'documentos': documentos,
+                   'cats': cats, 'desde': desde, 'hasta': hasta}
     else:
         context = {'categorias': categorias, 'documentos': documentos}
 
     return render(request, 'front/buscar.html', context)
+
 
 def handler404(request, exception):
     response = render(request, 'front/404.html', {})
     response.status_code = 403
     return response
 
+
 def handler403(request, exception):
     response = render(request, 'front/403.html', {})
     response.status_code = 403
     return response
 
-import sys
+
 def handler500(request):
     type_, value, traceback = sys.exc_info()
     response = render(request, 'front/500.html', {'value_': value})
